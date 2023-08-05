2e7b314f4f5eb212b6e5e26d71704af296ab4fea
SUNEEL PADALA <padalasuneel@gmail.com>
2023-08-06 00:20:33 +0530

    fixed calling one smart contract from another smart contract

--- College.sol
+++ College.sol
@@ -1,23 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.2 <0.9.0;
 import "Minting.sol";
 import "RegularBoard.sol";
 
 contract CollegeContract {
-    MintingContract public mint;
-    RegularBoardContract public regularBoardContract;
+    MintingContract mint;
 
-    address public collegeOwner;
+    address collegeOwner;
+    address regularSmartContractAddress;
+    address mintSmartContractAddress;
 
-    constructor() {
-        // Set the contract deployer's address as the owner
-        collegeOwner = msg.sender;
+
+    constructor(address _address, address _regular, address _mint) {
+        regularSmartContractAddress = _regular;
+        mintSmartContractAddress = _mint;
+        collegeOwner = _address;
     }
 
     modifier onlyCollege() {
         require(msg.sender == collegeOwner, "Only the College can access this function.");
         _; 
-    }
+    } 
 
     struct CollegeData {
         uint8 collegeId; 
@@ -33,68 +36,92 @@ contract CollegeContract {
     
     Course[] public CourseDetails; 
     mapping (address => CollegeData) colleMapping;
+    mapping (address => bool) collegeExists;
              //StudentId => courseId, marks
     mapping (uint8 => mapping(uint8 => uint8)) marksInfo;
             //studentId, courses
     mapping (uint8 => uint8[]) studentRegisteredCourses;
 
 
-    function displayBoards() public view returns(RegularBoardContract.BoardInfo[] memory) {
-        return regularBoardContract.getBoardList();
+    
+
+    function registerCollege(uint8 _collegeId, string memory _collegeName, uint8 _boardId) public returns (bool) {
+        if (collegeExists[collegeOwner]) {
+            return false;
+        }  
+        //Registe college with empty students
+        colleMapping[collegeOwner] = CollegeData(_collegeId, _collegeName, _boardId, new uint8[](0));
+        //update college id at Board
+        RegularInterface(regularSmartContractAddress).setCollege(_collegeId);
+        collegeExists[collegeOwner]=true;
+        return true;
+    }
+    function displayBoards() public view returns(RegularInterface.BoardInfo[] memory) {
+        return RegularInterface(regularSmartContractAddress).getBoardList();
     }
 
-    function registerCollege(address _collegeAddress, uint8 _collegeId, string memory _collegeName, uint8 _boardId) public {
-        //Registe college with empty students
-        colleMapping[_collegeAddress] = CollegeData(_collegeId, _collegeName, _boardId, new uint8[](0));
-        //update college id at Board
-        regularBoardContract.setCollege(_collegeId);
-    }
     function setStudent(uint8 _studentId) public {
-        CollegeData storage _collegeData = colleMapping[msg.sender];
-        //we can keep limit for number of colleges
+        CollegeData storage _collegeData = colleMapping[collegeOwner];
         _collegeData.studentIds.push(_studentId);
     }
     //addCourse() : colleges will add their courses :: working
-    function addCourse(uint8 _courseId, string memory _courseName) public onlyCollege returns (string memory){
+    function addCourse(uint8 _courseId, string memory _courseName) public returns (bool){
         for (uint i=0; i < CourseDetails.length; i++) {
             if (CourseDetails[i].courseId == _courseId) {
-                return "Fail";
+                return false;
             }
         }
         Course memory _course = Course(_courseId, _courseName);
         CourseDetails.push(_course);
-        return "Suceess";
+        return true;
     }
     //listCourses : To display at Student node : working
     function listCoures() external view returns (Course[] memory _courseList) {
         return CourseDetails;
     }
-    //working
-    function enrollCourse(uint8 _courseId, uint8 _studentId) public {
-        //keep condition for maximum students
+    //enrollCourse
+    function enrollCourse(uint8 _courseId, uint8 _studentId) public returns(bool) {
+        //studentId check
+        uint8[] memory _courseList = studentRegisteredCourses[_studentId];
+        for (uint i=0; i < _courseList.length; i++) {
+            if (_courseList[i] == _courseId) {
+                return false;
+            }
+        }
         studentRegisteredCourses[_studentId].push(_courseId);
+        return true;
     }
     //only college can add marks : working
-    function addMarks(uint8 _studentId, uint8 _courseId, uint8 _marks) public onlyCollege returns(string memory)  {
+    function addMarks(uint8 _studentId, uint8 _courseId, uint8 _marks) public returns(bool)  {
+        
         uint8[] memory _courseList = studentRegisteredCourses[_studentId];
         for (uint i=0; i < _courseList.length; i++) {
             if (_courseList[i] == _courseId) {
                 marksInfo[_studentId][_courseId] = _marks;
-                return  "Success";
+                return  true;
             }
         }
-        return "Fail";
+        return false;
     }
-    function generateTransacript(address _to, uint8 _studentId, string memory _studentName) public  onlyCollege {
+    function generateTransacript(address _to, uint8 _studentId, string memory _studentName) public returns(uint8)  {
         uint8 _marks = 0;
         uint8[] storage _coursesLists = studentRegisteredCourses[_studentId];
+        uint noOfCourses = _coursesLists.length;
+        if (noOfCourses <= 0) {
+            return 0;
+        }
         for (uint8 i = 0; i<_coursesLists.length; i++) {
             _marks = _marks + marksInfo[_studentId][_coursesLists[i]];
+        } 
+        string memory studentStatus = "";
+        if (_marks >= 35) {
+            studentStatus = "Passed";
+        } else {
+            studentStatus = "Failed";
         }
-        //keep condition for marks (pass fail)
-        mint.mintNFT(_to, _studentId, _studentName, _marks, "First Class");
+        return IMintingContract(mintSmartContractAddress).mintNFT(_to, _studentId, _studentName, colleMapping[collegeOwner].collegeName, _marks, studentStatus);
     }
-    function validateTransacript(uint8 tokenId) public view returns (uint8 _studentId, string memory _name, uint8 _marks, string memory _message) {
-        return mint.validateTranscript(tokenId);
+    function validateTransacript(uint8 tokenId) public view returns (uint8 _studentId, string memory _name, string memory college, uint8 _marks, string memory _message) {
+        return IMintingContract(mintSmartContractAddress).validateTranscript(tokenId);
     }
 }
--- RegularBoard.sol
+++ RegularBoard.sol
@@ -1,52 +1,59 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.2 <0.9.0;
 
-contract RegularBoardContract {
-    address public boardOwner;
-
-    constructor() {
-        // Set the contract deployer's address as the owner
-        boardOwner = msg.sender;
-    }
-
-    modifier onlyBoard() {
-        require(msg.sender == boardOwner, "Only the Board can access this function.");
-        _; 
-    }
+interface RegularInterface {
     struct Board {
         uint8 boardId; 
         string boardName;
         uint8[] listedColleges;
     }
+
     struct BoardInfo {
         uint8 boardId;
         string boardName;
+    }    
+
+    function setCollege(uint8 _collegeId) external ;
+    function getBoardList() external view returns (BoardInfo[] memory);
+    function getCollegeList() external view returns(uint8[] memory _collegeList) ;
+}
+
+
+contract RegularBoardContract is RegularInterface{
+    address boardOwner;
+    constructor(address _address) {
+        boardOwner = _address;
     }
-    BoardInfo[] public boardList;
+    modifier onlyBoard() {
+        require(msg.sender == boardOwner, "Only the Board can access this function.");
+        _; 
+    }
     mapping(address => Board) private boardMapping;
     mapping(address => bool) public boardDataExists;
+    BoardInfo[] public boardList;
+    
+    event sampleMsg(address);
 
     function registerBoard(uint8 _boardId, string memory _boardname) public returns (bool) {  
-        if (boardDataExists[msg.sender]) {
+        if (boardDataExists[boardOwner]) {
             return false;
-        }          
-        boardMapping[msg.sender] = Board(_boardId, _boardname, new uint8[](0));
-        boardDataExists[msg.sender] = true;
+        }                  
+        boardMapping[boardOwner] = Board(_boardId, _boardname, new uint8[](0));
+        boardDataExists[boardOwner] = true;
         BoardInfo memory _boardInfo = BoardInfo(_boardId, _boardname);
         boardList.push(_boardInfo);
         return true;
     }
 
-    function setCollege(uint8 _collegeId) public {
-        Board storage _board = boardMapping[msg.sender];
-        //we can keep limit for number of colleges
+    function setCollege(uint8 _collegeId) public  override  {
+        Board storage _board = boardMapping[boardOwner];         
         _board.listedColleges.push(_collegeId);
     }
-    function getBoardList() public view returns (BoardInfo[] memory) {        
+    function getBoardList() external view override returns (BoardInfo[] memory) {        
         return boardList;
     }
-    function getCollegeList() public view returns(uint8[] memory _collegeList) {
-        Board storage _board = boardMapping[msg.sender];
+    function getCollegeList() external view override returns(uint8[] memory _collegeList) {        
+        Board storage _board = boardMapping[boardOwner];
         return _board.listedColleges;
     }
 }
